8af1ea55c5bfea7c2858926f647c305bf38af9c4
vwheatle <vwheatle@kent.edu>
2022-10-19 01:58:10 -0400

    platonic ideal of all FPS games

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+	Transform head;
+	
+	Vector2 rotation;
+	
+	// TODO: there's a built-in mouse sensitivity thing in
+	//       the mouse 
+	public Vector2 sensitivity = new Vector2(1.2f, 1.2f);
+	public bool invertX = false, invertY = true;
+	
+	Vector2 invertVector() {
+		return new Vector2(
+			invertX ? -1f : 1f,
+			invertY ? -1f : 1f
+		);
+	}
+	
+	void Start() {
+		head = transform.Find("Head");
+		
+		// Lock cursor inside the window.
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void Update() {
+		// Based on reddit people here https://redd.it/8k7w7v but good
+		
+		Vector2 mouseMovement = new Vector2(
+			Input.GetAxisRaw("Mouse X"),
+			Input.GetAxisRaw("Mouse Y")
+		);
+		
+		rotation += mouseMovement * sensitivity * invertVector();
+		
+		// Disallow upside-down neck-breaking antics.
+		rotation.y = Mathf.Clamp(rotation.y, -90f, 90f);
+		
+		// These also override any other rotations to the player or head.
+		transform.localRotation = Quaternion.Euler(0f, rotation.x, 0f);
+		head     .localRotation = Quaternion.Euler(rotation.y, 0f, 0f);
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	CharacterController cc;
+	float moveSpeed = 4f;
+	
+	void Start() {
+		cc = GetComponent<CharacterController>();
+	}
+	
+	void Update() {
+		Vector2 wasd = new Vector2(
+			Input.GetAxisRaw("Horizontal"),
+			Input.GetAxisRaw("Vertical")
+		).normalized;
+		
+		Vector3 movement = new Vector3(wasd.x, -1f, wasd.y) * moveSpeed;
+		movement = transform.localRotation * movement;
+		
+		Vector3 gravity = new Vector3(0f, -1f, 0f);
+		movement += gravity;
+		
+		cc.Move(movement * Time.deltaTime);
+	}
+}
