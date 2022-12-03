6ae42a24b44e289be771329d2ad8d90055deef08
vwheatle <vwheatle@kent.edu>
2022-12-03 03:21:48 -0500

    soften textures and waypoint thing

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+	GameObject visual;
+	
+	void Awake() {
+		visual = this.transform.Find("Visual").gameObject;
+	}
+	
+	void Start() {
+		
+	}
+	
+	void Update() {
+		visual.transform.eulerAngles = new Vector3(
+			-75f, Time.time * 90f, 30f
+		);
+	}
+}
